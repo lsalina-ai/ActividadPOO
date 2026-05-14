910395b5c6ca617903288221232725707fd90d43
lsalina-ai <lsalina@unal.edu.co>
2026-05-14 01:41:53 -0500

    Agregar ejercicio 2

---
+++ src/Ejercicio2.java
@@ -0,0 +1,24 @@
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        double suma;
+        double x;
+        double y;
+
+        suma = 0;
+        x = 20;
+
+        suma = suma + x;
+
+        y = 40;
+
+        x = x + Math.pow(y, 2);
+
+        suma = suma + x / y;
+
+        System.out.println("EL VALOR DE LA SUMA ES: " + suma);
+
+    }
+
+}
